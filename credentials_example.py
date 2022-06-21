7ec9ed21874133f2e3a79ea4bbe7b77ec155286b
ccozens <ccozens@gmail.com>
2022-06-21 08:17:36 +0100

    Initial commit

---
+++ credentials_example.py
@@ -0,0 +1,6 @@
+'''
+To use, replace client_secret and client_secret (currently both random strings, **NOT** my credentials), and either (a) rename to "credentials.py" or (b) update api_request import statement to "from credentials_example import client_id, client_secret"
+'''
+
+client_id = 'yMVYwK'
+client_secret = '6nuR1zxnOaaUqckDRKcMuJV3b0oEVwiv'
